14add96a799138e33dbc12bf27b2e3d23c6d686b
Marco Herrn <marco@mherrn.de>
2023-01-16 16:58:28 +0100

    Move the Mute key in the function layer
    This is only to make room for another change that will result in a
    mapping on the old position of the Mute key.

--- hupfdule-model100/hupfdule-model100.ino
+++ hupfdule-model100/hupfdule-model100.ino
@@ -245,7 +245,7 @@ KEYMAPS(
    Consumer_ScanPreviousTrack, Key_F6,                 Key_F7,                   Key_F8,                   Key_F9,          Key_F10,          Key_F11,
    Consumer_PlaySlashPause,    Consumer_ScanNextTrack, Key_Escape,               Key_Tab,                  ___,             ___,              Key_F12,
                                Key_LeftArrow,          Key_DownArrow,            Key_UpArrow,              Key_RightArrow,  ___,              ___,
-   Key_PcApplication,          Consumer_Mute,          Consumer_VolumeDecrement, Consumer_VolumeIncrement, ___,             Key_Backslash,    Key_Pipe,
+   Key_PcApplication,          ___,                    Consumer_VolumeDecrement, Consumer_VolumeIncrement, Consumer_Mute,   Key_Backslash,    Key_Pipe,
    ___, ___, Key_Enter, ___,
    ___),
 
